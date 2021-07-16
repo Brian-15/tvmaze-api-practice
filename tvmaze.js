a5caca21e0afa85812607e42f4e6cdc9c34ed56c
Brian-15 <bcirieco@gmail.com>
2021-07-16 16:02:48 -0700

    added episode display button and implemented getter and populate funcs for episodes

--- tvmaze.js
+++ tvmaze.js
@@ -25,7 +25,6 @@ async function searchShows(query) {
   const shows = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`);
 
   for (let show of shows.data) {
-    console.log(show);
     result.push({
       id: show.show.id,
       name: show.show.name,
@@ -62,7 +61,22 @@ function populateShows(shows) {
     if (show.image) {
       $item.find('.card-body').append(`<img class="card-img-top" src="${show.image}" placeholder="">`)
     }
-    
+
+    // set up episode button on card body
+    $item.find('.card-body').append($('<button>')
+                            .attr('id', `${show.id}`)
+                            .text('Episodes')
+                            .addClass('show-episodes btn btn-primary')
+                            .click(async function () { // asynchronous event listener
+                              // get episode id
+                              const id = $(this).attr('id');
+
+                              // show episode area
+                              $('#episodes-area').show();
+
+                              // await episode data and populate list
+                              populateEpisodes(await getEpisodes(id));
+                            }));
     $showsList.append($item);
   }
 }
@@ -92,9 +106,40 @@ $("#search-form").on("submit", async function handleSearch (evt) {
  */
 
 async function getEpisodes(id) {
-  // TODO: get episodes from tvmaze
+  // get episodes from tvmaze
   //       you can get this by making GET request to
   //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
 
-  // TODO: return array-of-episode-info, as described in docstring above
+  const episodes = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`);
+  const result = [];
+
+  // push episode data to result array
+  for (let episode of episodes.data) {
+    result.push({
+      id: episode.id,
+      name: episode.name,
+      season: episode.season,
+      number: episode.number,
+    });
+  }
+
+  // return array-of-episode-info, as described in docstring above
+  return result;
 }
+
+/** populateEpisodes: renders episode list from array of episode information
+ * 
+ */
+
+function populateEpisodes(episodes) {
+  // empty episode list
+  const $episodesList = $('#episodes-list');
+  $episodesList.empty();
+
+  // iterate through episodes and append li elements to $episodesList
+  for (let episode of episodes) {
+    const $li = $('<li>').text(`${episode.name} (season ${episode.season}, number ${episode.number})`);
+    $episodesList.append($li);
+  }
+}
+
